7428cdc7a886bfd3b7523260d70648c41c648021
Saritahh <119914418+DeepRsnanceX@users.noreply.github.com>
2024-12-04 22:28:05 -0600

    Update TrailCutMod.hpp

--- include/TrailCutMod.hpp
+++ include/TrailCutMod.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #include <Geode/Bindings.hpp>
 #include <Geode/Modify.hpp>
+#include <Geode/loader/Mod.hpp>
+#include <Geode/utils/cocos.hpp>
 
 using namespace geode::prelude;
 
-class TrailCutMod : public Mod {
+class TrailCutMod {
 public:
     static inline float trailCutFrequency = 0.5f; // Default frequency (0.5 skips every other frame)
     static inline bool toggleTrailCutting = true; // Toggle cutting effect
 
-    void onLoad() override;
+    static void load(); // Called during mod loading
 };
